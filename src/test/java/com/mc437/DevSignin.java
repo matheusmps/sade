a23bb6aa8d06736c3996c1bdafab7bcca99d62d5
ronaldo-prata <roroprata@gmail.com>
2016-12-02 21:33:59 -0200

    Refatoração dos testes do Login Dev

--- src/test/java/com/mc437/DevSignin.java
+++ src/test/java/com/mc437/DevSignin.java
@@ -2,35 +2,71 @@ package com.mc437;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.mc437.stepdefs.StepDefs;
 
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class DevSignin extends StepDefs {
 		
+	private static final String rootUrl = "http://sample-env-2.v5vayj2nke.us-west-2.elasticbeanstalk.com/";
+	
+	@Before
+	public void init(){
+		File classpathRoot = new File(System.getProperty("user.dir"));
+		File chromedriver = new File(classpathRoot, "driver/chromedriver");
+		System.setProperty("webdriver.chrome.driver", chromedriver.getAbsolutePath());
+		driver = new ChromeDriver();
+	}
+
+	@After
+	public void tearDown(){
+		driver.close();
+	}
+
+	@When("Entrar na página login dev$")
+	public void openMembro(){
+		driver.get(rootUrl + "login");
+
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("rememberme")));
+	}
+	
 	@When("^coloco usuário \"([^\"]*)\" e senha \"([^\"]*)\"$")
 	public void user_pass_input(String user, String password){
-		WebElement email = driver.findElement(By.id("campo_email"));
+		WebElement email = driver.findElement(By.id("username"));
 		email.sendKeys(user);
-		WebElement senha = driver.findElement(By.id("campo_senha"));
+		WebElement senha = driver.findElement(By.id("pass"));
 		senha.sendKeys(password);	
-		WebElement acessar = driver.findElement(By.id("acessar"));
+		WebElement acessar = driver.findElement(By.tagName("button"));
 		acessar.click();
 	}
 	
-	@Then("^sou redirecionado para a página \"([^\"]*)\"$")
+	@Then("^sou redirecionado para a página de ([^\"]*)$")
 	public void check_page(String url){		
-		assertThat(driver.getCurrentUrl().equals(url));
+		assertThat(driver.getCurrentUrl().equals(rootUrl + url));
+	}
+	
+	@Then("estou na página do dev ([^\"]*)$")
+	public void ownership(String dev){
+		WebElement owner = driver.findElement(By.tagName("span"));
+		assertThat(owner.getText().equals("Olá " + dev));
 	}
 	
 	@Then("^é mostrada a mensagem de erro (.*)")
-	public void check_erro(String id){
-		WebElement errorbox = driver.findElement(By.id("erro"+id));
-		assertThat(errorbox.isDisplayed());
+	public void check_erro(String message){
+		WebElement errorbox = driver.findElement(By.className("alert"));
+		assertThat(errorbox.getText().equals(message));
 	}
 
 }
--- src/test/resources/com/mc437/loginDev.feature
+++ src/test/resources/com/mc437/loginDev.feature
@@ -1,31 +1,20 @@
 Feature: Entrar no sistema como um desenvolvedor do Sade.
 
     Scenario: login como usuário cadastrado
-        When Entrar na página 'http://localhost:8080/dev/'
-        When PENDING coloco usuário "renato@gmail.com" e senha "renato"
-        Then sou redirecionado para a página "http://localhost:8080/devperfil"
+        When Entrar na página login dev
+        When coloco usuário "Jo" e senha "jo"
+        Then sou redirecionado para a página de dev
+        And estou na página do dev Jo
 
 	Scenario: login com senha incorreta
-        When Entrar na página 'http://localhost:8080/dev/'
-		When PENDING coloco usuário "renato@gmail.com" e senha "123456"
-		Then sou redirecionado para a página "http://localhost:8080/devperfil"
-		And é mostrada a mensagem de erro 1
+        When Entrar na página login dev
+		When coloco usuário "Jo" e senha "naosousuasenha"
+		Then sou redirecionado para a página de login
+		And é mostrada a mensagem de erro "Username ou password inválido."
 	
 	Scenario: login com usuário não cadastrado
-        When Entrar na página 'http://localhost:8080/dev/'
-		When PENDING coloco usuário "renatogmail.com" e senha "123456"
-		Then sou redirecionado para a página "http://localhost:8080/devperfil"
-		And é mostrada a mensagem de erro 2
-	
-	Scenario: campo de email vazio
-        When Entrar na página 'http://localhost:8080/dev/'
-		When PENDING coloco usuário "" e senha "123456"
-		Then sou redirecionado para a página "http://localhost:8080/devperfil"
-		And é mostrada a mensagem de erro 3
-	
-	Scenario: campo de senha vazio
-        When Entrar na página 'http://localhost:8080/dev/'
-		When PENDING coloco usuário "renato@gmail.com" e senha ""
-		Then sou redirecionado para a página "http://localhost:8080/devperfil"
-		And é mostrada a mensagem de erro 3
+        When Entrar na página login dev
+		When coloco usuário "naosoudev" e senha "123456"
+		Then sou redirecionado para a página de login
+		And é mostrada a mensagem de erro "Username ou password inválido."
 	
